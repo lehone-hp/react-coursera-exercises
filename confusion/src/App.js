48804f432f37dbef9a23bb432d9176271d755d84
Lenya Hope <lehone4hope@gmail.com>
2020-07-08 15:48:30 +0100

    Confusion: Components Part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Confusion</NavbarBrand>
-        </div>
-      </Navbar>
+class App extends Component {
 
-      <Menu />
-    </div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Confusion</NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
